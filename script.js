8d271b47cd87a21406fc8fadf54c64816591a95e
Mitko-Yanev <62062395+Mitko-Yanev@users.noreply.github.com>
2021-02-05 13:49:44 +0100

    Sista versionen
    la in google quiz länkarna.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var random = new Array()
 
-random[0] = "https://www.youtube.com"
-random[1] = "https://twitter.com"
+random[0] = "https://docs.google.com/forms/d/e/1FAIpQLSfDy-Y6S5-r9sFpnTEvticctqCi044Ab23WXL3amcLzqg3v1g/viewform"
+random[1] = "https://docs.google.com/forms/d/e/1FAIpQLSe459plz6zPHweAD_5TwY5CtF29yjCIUWZHw72MHq-R4S5zgg/viewform"
 
-function generator() {
+function generator() 
+{
     window.location = random[Math.floor(Math.random() * random.length)];
-  }
+}
